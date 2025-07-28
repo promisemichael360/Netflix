3ff9e470ab0871704a81a1785b5698e4553bcb8d
mpromisenuel <mpromisenuel@gmail.com>
2025-07-28 13:21:22 +0000

    Portfolio

--- src/components/home_screen_component/Banner.jsx
+++ src/components/home_screen_component/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import axios from '../../axios'
+import axios from '../../../axios'
 import Request from '../../../request'
 import Spinner from '../Spinner'
 
